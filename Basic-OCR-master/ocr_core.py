8d4853d603b3e81f578e2cf5aed22c73483ecc06
Anurag G <54735168+Anuraganu98@users.noreply.github.com>
2020-08-20 23:04:00 +0530

    Add files via upload

---
+++ Basic-OCR-master/ocr_core.py
@@ -0,0 +1,33 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+import cv2
+# import keras
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 3)
+    # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    text = pytesseract.image_to_string(gray, lang='eng')
+    print(text, 'fun')
+    return text  # Then we will print the text in the image
+
+
+# model = keras.models.load_model('./model.hdf5')
+
+
+def ocr_core1(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    a = list('ABCDEFGHIJKLMONPQRSTUVWXYZ')
+    img = cv2.imread(filename)
+
+    text = pytesseract.image_to_string(img, lang='eng')
+    print(text, 'fun')
+    return text  # Then we will print the text in the image
